31a6cb6cd4ad1e8af2895c5908b2f97f65e5a002
Vicky do nascimento <101911203+Yads689@users.noreply.github.com>
2023-02-04 23:45:10 -0300

    att comit banco

--- blacktatoo (5).sql
+++ blacktatoo (5).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 05/02/2023 às 03:03
--- Versão do servidor: 10.4.22-MariaDB
--- Versão do PHP: 7.4.27
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Fev-2023 às 03:44
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -23,24 +23,28 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `blacktatoo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `blacktatoo`;
 
+CREATE USER IF NOT EXISTS 'tatoo'@'localhost' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON blacktatoo.* TO 'tatoo'@'localhost';
+
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `administrador`
+-- Estrutura da tabela `administrador`
 --
 
 DROP TABLE IF EXISTS `administrador`;
-CREATE TABLE `administrador` (
-  `cod_admin` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `cod_admin` int(50) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `data_nasc` varchar(11) NOT NULL,
-  `permissao` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `permissao` int(50) NOT NULL,
+  PRIMARY KEY (`cod_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `administrador`
+-- Extraindo dados da tabela `administrador`
 --
 
 INSERT INTO `administrador` (`cod_admin`, `nome`, `senha`, `email`, `data_nasc`, `permissao`) VALUES
@@ -49,47 +53,51 @@ INSERT INTO `administrador` (`cod_admin`, `nome`, `senha`, `email`, `data_nasc`,
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `agendamento`
+-- Estrutura da tabela `agendamento`
 --
 
 DROP TABLE IF EXISTS `agendamento`;
-CREATE TABLE `agendamento` (
-  `cod_agendamento` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `agendamento` (
+  `cod_agendamento` int(50) NOT NULL AUTO_INCREMENT,
   `autor` varchar(50) NOT NULL,
   `contato` varchar(50) NOT NULL,
   `cod_fun` varchar(50) NOT NULL,
   `data_agenda` varchar(10) NOT NULL,
   `estilo_valor` varchar(30) NOT NULL,
   `cod_cliente` varchar(10) NOT NULL,
-  `nome_cliente` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nome_cliente` varchar(10) NOT NULL,
+  PRIMARY KEY (`cod_agendamento`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `agendamento`
+-- Extraindo dados da tabela `agendamento`
 --
 
 INSERT INTO `agendamento` (`cod_agendamento`, `autor`, `contato`, `cod_fun`, `data_agenda`, `estilo_valor`, `cod_cliente`, `nome_cliente`) VALUES
-(1, 'Funcionario A', '123456789', '1', '1111-11-21', '', '2', 'Cliente 1');
+(1, 'Funcionario A', '123456789', '1', '1111-11-21', '', '2', 'Cliente 1'),
+(2, 'Funcionario A', '1234567897', '1', '2023-02-07', 'OLD SCHOOL - R$160', '1', 'Funcionari'),
+(3, 'Funcionario A', '(21)9911-22798', '1', '2023-02-26', 'TINTA BRANCA - R$400', '2', 'Funcionari');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cliente`
+-- Estrutura da tabela `cliente`
 --
 
 DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE `cliente` (
-  `cod_cliente` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `cod_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(50) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `data_nasc` varchar(11) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `permissao` int(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `permissao` int(50) NOT NULL,
+  PRIMARY KEY (`cod_cliente`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Despejando dados para a tabela `cliente`
+-- Extraindo dados da tabela `cliente`
 --
 
 INSERT INTO `cliente` (`cod_cliente`, `usuario`, `nome`, `email`, `data_nasc`, `senha`, `permissao`) VALUES
@@ -99,12 +107,12 @@ INSERT INTO `cliente` (`cod_cliente`, `usuario`, `nome`, `email`, `data_nasc`, `
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `funcionario`
+-- Estrutura da tabela `funcionario`
 --
 
 DROP TABLE IF EXISTS `funcionario`;
-CREATE TABLE `funcionario` (
-  `cod_func` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `cod_func` int(50) NOT NULL AUTO_INCREMENT,
   `apelido` varchar(50) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
@@ -112,11 +120,12 @@ CREATE TABLE `funcionario` (
   `tel` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
   `data_nasc` varchar(10) NOT NULL,
-  `permissao` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `permissao` int(50) NOT NULL,
+  PRIMARY KEY (`cod_func`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `funcionario`
+-- Extraindo dados da tabela `funcionario`
 --
 
 INSERT INTO `funcionario` (`cod_func`, `apelido`, `nome`, `senha`, `cpf`, `tel`, `email`, `data_nasc`, `permissao`) VALUES
@@ -127,86 +136,28 @@ INSERT INTO `funcionario` (`cod_func`, `apelido`, `nome`, `senha`, `cpf`, `tel`,
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tatuagem`
+-- Estrutura da tabela `tatuagem`
 --
 
 DROP TABLE IF EXISTS `tatuagem`;
-CREATE TABLE `tatuagem` (
-  `cod_tatto` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tatuagem` (
+  `cod_tatto` int(50) NOT NULL AUTO_INCREMENT,
   `estilo` varchar(50) NOT NULL,
   `preco` varchar(50) NOT NULL,
   `autor` varchar(50) NOT NULL,
   `link` varchar(50) NOT NULL,
-  `cod_func` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `cod_func` int(50) NOT NULL,
+  PRIMARY KEY (`cod_tatto`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tabelas despejadas
+-- Extraindo dados da tabela `tatuagem`
 --
 
---
--- Índices de tabela `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`cod_admin`);
-
---
--- Índices de tabela `agendamento`
---
-ALTER TABLE `agendamento`
-  ADD PRIMARY KEY (`cod_agendamento`);
-
---
--- Índices de tabela `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cod_cliente`);
-
---
--- Índices de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`cod_func`);
-
---
--- Índices de tabela `tatuagem`
---
-ALTER TABLE `tatuagem`
-  ADD PRIMARY KEY (`cod_tatto`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `administrador`
---
-ALTER TABLE `administrador`
-  MODIFY `cod_admin` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `agendamento`
---
-ALTER TABLE `agendamento`
-  MODIFY `cod_agendamento` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `cod_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `cod_func` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de tabela `tatuagem`
---
-ALTER TABLE `tatuagem`
-  MODIFY `cod_tatto` int(50) NOT NULL AUTO_INCREMENT;
+INSERT INTO `tatuagem` (`cod_tatto`, `estilo`, `preco`, `autor`, `link`, `cod_func`) VALUES
+(1, 'TINTA BRANCA', '400', 'Funcionario A', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:AN', 0),
+(2, 'OLD SCHOOL', '160', 'Funcionario A', 'https://i.pinimg.com/236x/d0/77/fe/d077fe74cc94591', 0),
+(3, 'SINGLE LINE', '60', 'Funcionario A', 'https://i.pinimg.com/564x/6b/d7/9e/6bd79e77a00d52e', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
