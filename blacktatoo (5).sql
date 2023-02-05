238b42180c00219b857a29716322c3d52c304649
Yads689 <101911203+Yads689@users.noreply.github.com>
2023-02-04 21:38:15 -0300

    Update blacktatoo (5).sql

--- blacktatoo (5).sql
+++ blacktatoo (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 14/01/2023 às 01:32
+-- Tempo de geração: 05/02/2023 às 01:23
 -- Versão do servidor: 10.4.22-MariaDB
 -- Versão do PHP: 7.4.27
 
@@ -23,9 +23,6 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `blacktatoo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `blacktatoo`;
 
-CREATE USER IF NOT EXISTS 'tatoo'@'localhost' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON blacktatoo.* TO 'tatoo'@'localhost';
-
 -- --------------------------------------------------------
 
 --
@@ -34,7 +31,7 @@ GRANT ALL PRIVILEGES ON blacktatoo.* TO 'tatoo'@'localhost';
 
 DROP TABLE IF EXISTS `administrador`;
 CREATE TABLE IF NOT EXISTS `administrador` (
-  `cod_admin` int (50) NOT NULL AUTO_INCREMENT,
+  `cod_admin` int(50) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -44,11 +41,38 @@ CREATE TABLE IF NOT EXISTS `administrador` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
+-- RELACIONAMENTOS PARA TABELAS `administrador`:
+--
+
+--
 -- Despejando dados para a tabela `administrador`
 --
 
-INSERT INTO `administrador` (`cod_admin`, `nome`, `senha`, `email`, `data_nasc`, `permissao`) VALUES 
-('1', 'black_adm', '$2y$10$wrmriMXv8TlN4k/FF3hVu.Ylz8H9navNly.n7gYFghs4Yr7IwEvRG', 'blacktatto@gmail.com', '01-01-2022', '1');
+INSERT INTO `administrador` (`cod_admin`, `nome`, `senha`, `email`, `data_nasc`, `permissao`) VALUES
+(1, 'black_adm', '$2y$10$wrmriMXv8TlN4k/FF3hVu.Ylz8H9navNly.n7gYFghs4Yr7IwEvRG', 'blacktatto@gmail.com', '01-01-2022', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `agendamento`
+--
+
+DROP TABLE IF EXISTS `agendamento`;
+CREATE TABLE IF NOT EXISTS `agendamento` (
+  `cod_agendamento` int(50) NOT NULL AUTO_INCREMENT,
+  `autor` varchar(50) NOT NULL,
+  `contato` varchar(50) NOT NULL,
+  `cod_fun` varchar(50) NOT NULL,
+  `data_agenda` varchar(10) NOT NULL,
+  `estilo_valor` varchar(30) NOT NULL,
+  `cod_cliente` varchar(10) NOT NULL,
+  `nome_cliente` varchar(10) NOT NULL,
+  PRIMARY KEY (`cod_agendamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELACIONAMENTOS PARA TABELAS `agendamento`:
+--
 
 -- --------------------------------------------------------
 
@@ -69,6 +93,10 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
+-- RELACIONAMENTOS PARA TABELAS `cliente`:
+--
+
+--
 -- Despejando dados para a tabela `cliente`
 --
 
@@ -83,59 +111,43 @@ INSERT INTO `cliente` (`cod_cliente`, `usuario`, `nome`, `email`, `data_nasc`, `
 
 DROP TABLE IF EXISTS `funcionario`;
 CREATE TABLE IF NOT EXISTS `funcionario` (
-  `cod_func` int (50) NOT NULL AUTO_INCREMENT,
+  `cod_func` int(50) NOT NULL AUTO_INCREMENT,
   `apelido` varchar(50) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `cpf` varchar(16) NOT NULL,
-  `tel` varchar(15) NOT NULL,  
+  `tel` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
   `data_nasc` varchar(10) NOT NULL,
   `permissao` int(50) NOT NULL,
   PRIMARY KEY (`cod_func`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
 
 --
--- Despejando dados para a tabela `funcionario`
+-- RELACIONAMENTOS PARA TABELAS `funcionario`:
 --
 
-INSERT INTO `funcionario` (`cod_func`, `apelido`, `nome`, `senha`, `cpf`, `tel`, `email`, `data_nasc`, `permissao`) VALUES (NULL, 'black', 'black funcionario', '$2y$10$tVEJ6ZKMUaDP.O3vHW5yOuS8tsoGuUZnMqPrw5T7z4374oHeDh3s6', '14525636510', '(21) 9 9911-227', 'blackfunc@gmail.com', '2023-02-01', '2')
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `agendamento`
---
-DROP TABLE IF EXISTS `agendamento`;
-CREATE TABLE IF NOT EXISTS `agendamento` (
-  `cod_agendamento` int (50) AUTO_INCREMENT,
-  `autor` varchar (50) NOT NULL,
-  `contato` varchar(50) NOT NULL,
-  `cod_fun` varchar(50) NOT NULL,
-  `data_agenda` varchar(10) NOT NULL,
-  `estilo_valor` varchar(30) NOT NULL,
-  `cod_cliente` varchar(10) NOT NULL,
-  `nome_cliente` varchar(10) NOT NULL,
-  PRIMARY KEY (`cod_agendamento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
 -- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `tatuagem`
 --
 
+DROP TABLE IF EXISTS `tatuagem`;
 CREATE TABLE IF NOT EXISTS `tatuagem` (
-`cod_tatto` int (50) AUTO_INCREMENT,
-`estilo` varchar (50) NOT NULL,
-`preco` varchar (50) NOT NULL,
-`autor` varchar (50) NOT NULL,
-`link` varchar (50) NOT NULL,
-`cod_func` int (50) NOT NULL,
-PRIMARY KEY (`cod_tatto`)
-) 
+  `cod_tatto` int(50) NOT NULL AUTO_INCREMENT,
+  `estilo` varchar(50) NOT NULL,
+  `preco` varchar(50) NOT NULL,
+  `autor` varchar(50) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `cod_func` int(50) NOT NULL,
+  PRIMARY KEY (`cod_tatto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- RELACIONAMENTOS PARA TABELAS `tatuagem`:
+--
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
